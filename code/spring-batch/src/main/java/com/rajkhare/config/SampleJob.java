a4eca2f984a5186ecb256f5ae892dcfa148c6aca
raj.khare@oracle.com <raj.khare@oracle.com>
2024-12-10 05:26:55 +0530

    updatew0:10-12-2024

--- code/spring-batch/src/main/java/com/rajkhare/config/SampleJob.java
+++ code/spring-batch/src/main/java/com/rajkhare/config/SampleJob.java
@@ -7,6 +7,7 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -29,6 +30,7 @@ public class SampleJob {
     @Bean
     public Job firstJob() {
         return jobBuilderFactory.get("First Job")
+                .incrementer(new RunIdIncrementer())
                 .start(firstStep())
                 .next(secondStep())
                 .build();
